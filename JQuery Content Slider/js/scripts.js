50891e1d3325cb4e10155bb193df745702be3152
Austin Akers <austin.akers5@gmail.com>
2016-02-13 09:01:50 -0600

    Added JavaScript Functionality

--- JQuery Content Slider/js/scripts.js
+++ JQuery Content Slider/js/scripts.js
@@ -1,13 +1,66 @@
 //JQuery
 $(document).ready(function()
 {
-	$('.myLink').on('mouseenter', function(){
-			$('h1').addClass('red');
-			$('h1').slideUp(500);
-	});
-		$('.myLink').on('mouseleave', function(){
-			$('h1').removeClass('red');
-			$('h1').slideDown(500);
-	});
+	//Set Options
 
+	var speed = 500; 			//fade speed
+	var autoSwitch = true;		//auto slider options
+	var autoSwitchSpeed = 4000; //Auto slider speed
+
+	//Add initial active class
+	$('.slide').first().addClass('active');
+
+
+	//Hide all slides
+	$('.slide').hide();
+
+
+	//Show first slide
+	$('.active').show();
+
+	//Next handler
+	$('#next').on('click', nextSlide);
+
+		$('#prev').on('click', prevSlide);
+
+	//Auto slider	
+	if(autoSwitch == true)
+	{
+		setInterval (nextSlide, autoSwitchSpeed);
+	}
+
+
+	//Switch to the next slide
+	function nextSlide()
+	{
+		$('.active').removeClass('active').addClass('oldActive');
+		if($('.oldActive').is(':last-child'))
+		{
+			$('.slide').first().addClass('active');
+		}
+		else
+		{
+			$('.oldActive').next().addClass('active');
+		}
+		$('.oldActive').removeClass('oldActive');
+		$('.slide').fadeOut(speed);
+		$('.active').fadeIn(speed);
+	}
+
+	//Switch to prev slide
+	function prevSlide()
+	{
+		$('.active').removeClass('active').addClass('oldActive');
+		if($('.oldActive').is(':first-child'))
+		{
+			$('.slide').last().addClass('active');
+		}
+		else
+		{
+			$('.oldActive').prev().addClass('active');
+		}
+		$('.oldActive').removeClass('oldActive');
+		$('.slide').fadeOut(speed);
+		$('.active').fadeIn(speed);
+	}
 });
