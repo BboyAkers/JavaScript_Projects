63de2db625d5147c1519e95512831240f3500f49
Austin Akers <austin.akers5@gmail.com>
2016-02-17 00:41:03 -0600

    Added Part of JS functionality

---
+++ Youtube Search Engine/js/scripts.js
@@ -0,0 +1,31 @@
+// Searchbar Handler
+$(function(){
+	var searchField = $('#query');
+	var icon = $('#search-btn');
+	
+	// Focus Event Handler
+	$(searchField).on('focus', function(){
+		$(this).animate({
+			width: '100%'
+		}, 400);
+		$(icon).animate({
+			right: '10px'
+		}, 400);
+	});
+	
+	// Blur Event Handler
+	$(searchField).on('blur', function(){
+		if(searchField.val() == ''){
+			$(searchField).animate({
+				width:'45%'
+			},400, function(){});
+			$(icon).animate({
+				right:'360px'
+			},400, function(){});
+		}
+	});
+	
+	$('#search-form').submit(function(e){
+		e.preventDefault();
+	});
+});
