d24d8c7bef0dcf9f17fb76ec5c9c74047cb38883
Austin Akers <aakers@ambitenergy.com>
2017-06-10 16:50:45 -0500

    adding fccproject

---
+++ freeCodeCampQuickProject/js/scripts.js
@@ -0,0 +1,13 @@
+var changeInnerText = document.getElementById('changeText');
+
+var blueBackground = document.getElementById('blueBackground');
+
+function changeText() {
+    changeInnerText.value = 'Wadup Joe';
+    changeInnerText.innerHTML = 'Fluffy bunnies';
+}
+
+function changeBackground() {
+    
+
+}
