302dcfb8e8c6c125f02fb7ec213909710341f6a7
Austin Akers <austin.akers5@gmail.com>
2018-01-11 22:24:45 -0600

    added first coin

---
+++ cryptoWatchApp/js/scripts.js
@@ -0,0 +1,20 @@
+const ranking = document.querySelector('.cryptoRank');
+const title = document.querySelector('.cryptoTitle');
+const subTitle = document.querySelector('.cryptoSubTitle');
+const price = document.querySelector('.cryptoPrice');
+
+function getCrypto() {
+  fetch('https://api.coinmarketcap.com/v1/ticker/bitcoin/')
+    .then(function(Result) {
+      return Result.json();
+    }).then(function(Result) {
+      console.log(Result);
+      ranking.innerHTML = Result[0].rank;
+      title.innerHTML = Result[0].name;
+      subTitle.innerHTML = Result[0].symbol;
+      price.innerHTML = Result[0].price_usd;
+      console.log(Result[0].id);
+    });
+}
+
+getCrypto();
