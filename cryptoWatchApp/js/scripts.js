0135a4f7be755a44c2dc7ec7739e4f97ac811ac4
Austin Akers <austin.akers5@gmail.com>
2018-01-14 00:02:50 -0600

    push code

--- cryptoWatchApp/js/scripts.js
+++ cryptoWatchApp/js/scripts.js
@@ -1,20 +1,26 @@
-const ranking = document.querySelector('.cryptoRank');
-const title = document.querySelector('.cryptoTitle');
-const subTitle = document.querySelector('.cryptoSubTitle');
-const price = document.querySelector('.cryptoPrice');
 
-function getCrypto() {
-  fetch('https://api.coinmarketcap.com/v1/ticker/bitcoin/')
-    .then(function(Result) {
-      return Result.json();
-    }).then(function(Result) {
-      console.log(Result);
-      ranking.innerHTML = Result[0].rank;
-      title.innerHTML = Result[0].name;
-      subTitle.innerHTML = Result[0].symbol;
-      price.innerHTML = Result[0].price_usd;
-      // console.log(Result[0].id);
-    });
-}
-
-getCrypto();
+document.addEventListener("DOMContentLoaded", function() {
+  const ranking = document.querySelector('.cryptoRank');
+  const title = document.querySelector('.cryptoTitle');
+  const subTitle = document.querySelector('.cryptoSubTitle');
+  const price = document.querySelector('.cryptoPrice');
+  
+  function getCrypto() {
+    fetch('https://api.coinmarketcap.com/v1/ticker/')
+      .then(function(Results) {
+        return Results.json();
+      }).then(function(Results) {
+        console.log(Results);
+        let card = doucment.querySelector('.card');
+          Results.forEach(item => {
+            card.push
+          });
+          ranking.innerHTML = Result.rank;
+          title.innerHTML = Result.name;
+          subTitle.innerHTML = Result.symbol;
+          price.innerHTML = Result.price_usd;
+        
+        // console.log(Result[0].id);
+      });
+  }
+});
