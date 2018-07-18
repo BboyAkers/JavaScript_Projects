fb5ff4fb79c08ba7a4f3bfce49d1189ab84d737d
Austin Akers <austin.akers5@gmail.com>
2018-07-17 22:47:13 -0500

    pushing code to work on in the morning

--- DOMTraversalPractice/js/scripts.js
+++ DOMTraversalPractice/js/scripts.js
@@ -1,16 +1,15 @@
 // STARTING POINT
 const list = document.querySelector('.list');
-
 // 1: Store the first child of the `ul` in the variable `firstItem`
-const firstItem;
+const firstItem = list.firstElementChild;
 firstItem.style.backgroundColor = '#04c5e6';
 
 // 2: Using traversal, store the second list item in a variable named `nextItem`
-
+// const secondItem = firstItem.nextElementSibling;
 nextItem.style.backgroundColor = '#b7c7d0';
 
 // 3: Store the last child of the `ul` in a variable named `lastItem`
-
+// const lastItem = list.lastElementChild;
 lastItem.style.backgroundColor = '#57d6ab';
 
 // 4: Using traversal, store the second-to-last list item in a variable named `prevItem`
@@ -26,7 +25,7 @@ banner.className = 'banner';
 wrapper.style.backgroundColor = '#fcfcfc';
 
 // 7: Using traversal, store the body in a variable named `body`
-
+// const body = document.querySelector('body');
 body.style.backgroundColor = '#f8fdf3';
 
 
