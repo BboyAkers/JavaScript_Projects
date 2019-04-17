1976d5b039de83f18fa43d1791553848b234af56
Austin Akers <austin.akers5@gmail.com>
2019-04-17 17:59:38 -0500

    added httprequest

---
+++ GithubSearch/js/scripts.js
@@ -0,0 +1,18 @@
+
+const searchUser = document.querySelector('.searchUser');
+const userCardInfo = document.querySelector('.userInfoCard');
+
+searchUser.addEventListener('click', (e) =>{
+    e.preventDefault();
+    
+    let usernameInputValue = document.querySelector('.usernameInput').value;
+    const xhrRequest = new XMLHttpRequest();
+
+    xhrRequest.open("GET", `https://api.github.com/users/${usernameInputValue}`);
+    xhrRequest.onreadystatechange = function() {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            console.log(this.response)
+        }
+    }
+    xhrRequest.send();
+});
