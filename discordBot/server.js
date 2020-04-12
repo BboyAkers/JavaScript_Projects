b6e964a50af0360e19e1b0d5edd5e323cef38336
unknown <austin.akers5@gmail.com>
2020-04-11 20:58:58 -0500

    simple bot

--- discordBot/server.js
+++ discordBot/server.js
@@ -1,14 +1,27 @@
+require('dotenv').config();
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+bot.on('message', msg => {
+  if(msg.content === 'ping') {
+    //replies back to the person with their @username
+    msg.reply('pong');
+    //Just say it in the channel no tagging usernames
+    msg.channel.send('pong');
+  } else if (msg.content.startsWith('!rating')) {
+    if(msg.mentions.users.size) {
+      const taggedUser = msg.mentions.user.first();
+      msg.reply(`The user rating for ${taggedUser.username} is  98%`)
+    } else {
+      msg.reply('Please tag a valid user!');
+    }
   }
-});
-
-client.login('token');
+})
