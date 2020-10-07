e90adcbf785ba215fe6ffe0606682ac95c7b8f37
unknown <austin.akers5@gmail.com>
2020-10-07 00:30:23 -0700

    pushing tssort project with bubble sort function

--- TSSort/src/index.ts
+++ TSSort/src/index.ts
@@ -1,1 +1,22 @@
-console.log('wasup')
+class Sorter {
+
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const length = this.collection.length;
+
+    for (let i = 0; i < length; i++) {
+      for(let j = 0; j < length - i - 1; j++) {
+        if(this.collection[j] > this.collection[j + 1]){
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j+1] = temp;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
