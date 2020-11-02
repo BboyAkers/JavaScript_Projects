fb91ff11270b3f7192634bff0b1743b1be096339
unknown <austin.akers5@gmail.com>
2020-11-01 22:39:14 -0800

    Refactored the swapping and comparison

---
+++ TSSort/src/Sorter.ts
@@ -0,0 +1,17 @@
+import { NumbersCollection } from './NumbersCollection';
+export class Sorter {
+
+  constructor(public collection: NumbersCollection) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for(let j = 0; j < length - i - 1; j++) {
+        if(this.collection.compare(j, j + 1)) {
+          this.collection.swap(j, j + 1);
+        }
+      }
+    }
+  }
+}
