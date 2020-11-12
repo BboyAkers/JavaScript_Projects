f924711bc2a2e0b2c35e92426d86e7083ad37897
unknown <austin.akers5@gmail.com>
2020-11-12 07:11:14 -0800

    finished TSSorting

--- TSSort/src/index.ts
+++ TSSort/src/index.ts
@@ -1,13 +1,20 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([10, 3, -5, 0])
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('Austin');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data)
+charactersCollection.sort();
+console.log(charactersCollection);
+
+const linkedList = new LinkedList();
+linkedList.add(24);
+linkedList.add(-50);
+linkedList.add(30);
+linkedList.add(6);
+
+linkedList.sort();
+linkedList.print();
