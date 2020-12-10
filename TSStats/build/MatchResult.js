5134fcb62958c33f4e33818449762103cdfbe18e
unknown <austin.akers5@gmail.com>
2020-12-09 23:06:10 -0800

    added enum, described row with tuple, ect.

---
+++ TSStats/build/MatchResult.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MatchResult = void 0;
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult = exports.MatchResult || (exports.MatchResult = {}));
--- TSStats/src/CsvFileReader.ts
+++ TSStats/src/CsvFileReader.ts
@@ -1,7 +1,11 @@
 import fs from 'fs';
+import { MatchResult } from './MatchResult';
+import { dateStringToDate } from './utils';
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
 
 export class CsvFileReader {
-  data: string[][] = [];
+  data: MatchData[] = [];
 
   constructor(public fileName: string) {}
 
@@ -10,6 +14,16 @@ export class CsvFileReader {
       encoding: 'utf-8'
     }).split('\n').map((row: string): string[] =>{
       return row.split(',');
+    }).map((row: string[]): MatchData => {
+      return [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        row[5] as MatchResult,
+        row[6]
+      ];
     });
   }
 }
