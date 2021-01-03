756679d823ab2048f477a1f52f78d70d966c2461
unknown <austin.akers5@gmail.com>
2021-01-02 18:13:39 -0800

    refactored csv reader

--- TSStats/src/CsvFileReader.ts
+++ TSStats/src/CsvFileReader.ts
@@ -1,29 +1,16 @@
 import fs from 'fs';
-import { MatchResult } from './MatchResult';
-import { dateStringToDate } from './utils';
-
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-export class CsvFileReader {
-  data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public fileName: string) {}
+
+  abstract mapRow(row: string[]): T;
 
   read():void {
     this.data = fs.readFileSync(this.fileName, {
       encoding: 'utf-8'
     }).split('\n').map((row: string): string[] =>{
       return row.split(',');
-    }).map((row: string[]): MatchData => {
-      return [
-        dateStringToDate(row[0]),
-        row[1],
-        row[2],
-        parseInt(row[3]),
-        parseInt(row[4]),
-        row[5] as MatchResult,
-        row[6]
-      ];
-    });
+    }).map(this.mapRow);
   }
 }
