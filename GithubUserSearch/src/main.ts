e5754b2e3b11ea7ccac6846473a021f1283c0a96
Austin Akers <austinakers@Austins-MacBook-Pro.local>
2024-01-02 19:29:23 -0800

    finished github search challenge

--- GithubUserSearch/src/main.ts
+++ GithubUserSearch/src/main.ts
@@ -40,24 +40,24 @@ const renderTemplate = (data: GithubUser) => {
         <img class="col-span-3 md:col-span-3 rounded-full inline max-h-[100px] md:max-h-[150px]" src=${avatar_url}
           alt="Github profile picture" />
         <div class="flex flex-col justify-center col-span-5 ml-2 md:ml-8 md:col-span-9">
-          <h2 class="font-semibold md:text-2xl">${name}</h2>
+          <h2 class="font-semibold text-gray-dark dark:text-white-light md:text-2xl">${name}</h2>
           <a href=${html_url} target="_blank" class="text-blue-500">@${login}</a>
           <p class="text-sm">Joined ${accountCreationDate.toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric'})}</p>
         </div>
       </div>
       <p class="inline-block mt-8 text-sm">${bio}</p>
-      <div class="grid grid-cols-3 px-2 py-4 my-6 text-center bg-[#F6F8FF] rounded-xl">
+      <div class="grid grid-cols-3 px-2 py-4 my-6 text-center bg-white-dark dark:bg-navyBlue rounded-xl">
         <div>
-          <p class="text-sm text-[#4B6A9B]">Repos</p>
-          <p class="font-bold">${public_repos}</p>
+          <p class="text-sm text-blue dark:text-white-light">Repos</p>
+          <p class="font-bold text-gray-dark dark:text-white-light">${public_repos}</p>
         </div>
         <div>
-          <p class="text-sm text-[#4B6A9B]">Followers</p>
-          <p class="font-bold">${followers}</p>
+          <p class="text-sm text-blue dark:text-white-light">Followers</p>
+          <p class="font-bold text-gray-dark dark:text-white-light">${followers}</p>
         </div>
         <div>
-          <p class="text-sm text-[#4B6A9B]">Following</p>
-          <p class="font-bold">${following}</p>
+          <p class="text-sm text-blue dark:text-white-light">Following</p>
+          <p class="font-bold text-gray-dark dark:text-white-light">${following}</p>
         </div>
       </div>
       <div class="grid grid-cols-1 md:grid-cols-2 text-sm">
@@ -65,14 +65,14 @@ const renderTemplate = (data: GithubUser) => {
           <p class="mt-3">
             <img src="https://imagedelivery.net/nGYhisqu4x6SCDrz5V8Qxg/93c14a4f-2304-41d5-5c82-f1106df08600/public" alt="Location point icon" class="inline-block mr-5" />${location ? location : 'Not Available'}
           </p>
-          <a href=${blog} target="_blank" class="mt-3 block">
+          <a href=${blog} target="_blank" class="mt-3 block hover:underline">
             <img src="https://imagedelivery.net/nGYhisqu4x6SCDrz5V8Qxg/e7153a57-56da-45d3-3f00-5cfc2a1fdd00/public" alt="Link Icon for website" class="inline-block mr-4" />${blog ? blog : 'Not Available'}
           </a>
         </div>
         <div>
           <p class="mt-3">
             <img src="https://imagedelivery.net/nGYhisqu4x6SCDrz5V8Qxg/1455261b-d72c-4bb1-2389-401536ac8d00/public" alt="Twitter/x.com icon"
-              class="inline-block mr-4" />${twitter_username ? twitter_username : 'Not Available'}
+              class="inline-block mr-4" />${twitter_username ? `@${twitter_username}` : 'Not Available'}
           </p>
           <p class="mt-3">
             <img src="https://imagedelivery.net/nGYhisqu4x6SCDrz5V8Qxg/aee7aad6-5a36-44a0-72b1-e79666c90700/public" alt="Building icon for company"
