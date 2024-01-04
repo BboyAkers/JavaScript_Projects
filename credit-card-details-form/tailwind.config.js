786b3f847a6833ba2b413d9e6780568673efbd6b
Austin Akers <austinakers@Austins-MacBook-Pro.local>
2024-01-03 16:05:52 -0800

    finished cc form for mobile

--- credit-card-details-form/tailwind.config.js
+++ credit-card-details-form/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{svelte,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'main-mobile': "url('https://imagedelivery.net/nGYhisqu4x6SCDrz5V8Qxg/27413fd2-992a-4cb4-d75e-e10990957600/public')",
+        'main-desktop': "url('https://imagedelivery.net/nGYhisqu4x6SCDrz5V8Qxg/27413fd2-992a-4cb4-d75e-e10990957600/public')",
+      }
+    },
   },
   plugins: [],
 }
