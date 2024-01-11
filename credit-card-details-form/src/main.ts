446b6adb8af7551bd6228419dbdb6ab0f686ce64
Austin Akers <austinakers@Austins-MacBook-Pro.local>
2024-01-10 16:19:29 -0800

    removed custom check code and imported payment package

--- credit-card-details-form/src/main.ts
+++ credit-card-details-form/src/main.ts
@@ -1,4 +1,6 @@
 import './style.css'
+import Payment from 'payment';
+
 document.addEventListener('DOMContentLoaded', () => {
   const cardNumber = document.querySelector('#card-number') as HTMLInputElement;
   const cardHolder = document.querySelector('#card-holder') as HTMLInputElement;
@@ -6,38 +8,29 @@ document.addEventListener('DOMContentLoaded', () => {
   const cardExpirationYear = document.querySelector('#card-expiration-year') as HTMLInputElement;
   const cardCvv = document.querySelector('#card-cvv') as HTMLInputElement;
 
-  const cardNumberDisplay = document.querySelector('#card-number-display') as HTMLParagraphElement;
-  const cardHolderDisplay = document.querySelector('#card-holder-display') as HTMLParagraphElement;
+  const cardNumberDisplay = document.querySelector('#card-number-display') as HTMLSpanElement;
+  const cardHolderDisplay = document.querySelector('#card-holder-display') as HTMLSpanElement;
   const cardExpirationMonthDisplay = document.querySelector('#card-month-display') as HTMLSpanElement;
   const cardExpirationYearDisplay = document.querySelector('#card-year-display') as HTMLSpanElement;
-  const cardCvvDisplay = document.querySelector('#card-cvv-display') as HTMLParagraphElement;
+  const cardCvvDisplay = document.querySelector('#card-cvv-display') as HTMLSpanElement;
+  const cardTypeDisplay = document.querySelector('#card-type-display') as HTMLSpanElement;
 
-  const reactiveInputToDisplay = (input: HTMLInputElement, display: HTMLElement) => {
-    input.addEventListener('keyup', () => {
-      if (input.id === 'card-number') {
-        input.value = formatCreditCardNumber(input.value);
-      }
-      if(input.value.length > 0 ){
-        display.innerHTML = input.value;
-      }
-    });
-  }
-
-  const formatCreditCardNumber = (value: string) => {
-    const v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '');
-    const matches = v.match(/\d{4,16}/g);
-    const match = matches && matches[0] || '';
-    const parts = [];
-    for (let i = 0, len = match.length; i < len; i += 4) {
-        parts.push(match.substring(i, i + 4));
-    }
-    if (parts.length) {
-        return parts.join(' ');
-    }
-    return value;
+  const getCardType = (cardNumber: string) => {
+    return Payment.fns.cardType(cardNumber);
   };
 
-  reactiveInputToDisplay(cardNumber, cardNumberDisplay);
+  const reactiveInputToDisplay = (input: HTMLInputElement, display: HTMLElement) => {
+    const defaultValue = display.textContent;
+    input.addEventListener('input', () => {
+      if(input.id === 'card-number') {
+        cardTypeDisplay.textContent = getCardType(input.value) ?? '';
+      }
+        display.textContent = input.value || defaultValue;
+    });
+  };
+
+
+  reactiveInputToDisplay(Payment.formatCardNumber(cardNumber), cardNumberDisplay);
   reactiveInputToDisplay(cardHolder, cardHolderDisplay);
   reactiveInputToDisplay(cardExpirationMonth, cardExpirationMonthDisplay);
   reactiveInputToDisplay(cardExpirationYear, cardExpirationYearDisplay);
