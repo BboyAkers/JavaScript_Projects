c0b81e517091a1c24d27bd9039d7c476f0dd3b5b
Austin Akers <austin.akers5@gmail.com>
2024-05-07 22:15:15 -0700

    added copy svg to tab index and got all form elements

--- password-generator/src/main.ts
+++ password-generator/src/main.ts
@@ -1,1 +1,11 @@
 import './style.css'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#passwordForm')! as HTMLFormElement;
+
+  const formElements = Array.from(form.elements);
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(formElements)
+  });
+});
