16b0e5f28666c7cfe771be979571cfdee9e99740
Austin Akers <austin.akers5@gmail.com>
2024-09-04 14:27:25 -0700

    set up inital tailwind configuration and styling for the project

--- planets-facts/tailwind.config.js
+++ planets-facts/tailwind.config.js
@@ -5,7 +5,42 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'stars': "url('/assets/background-stars.svg')"
+      },
+      colors: {
+        white: {
+          DEFAULT: '#FFFFFF',
+        },
+        blue: {
+          light: '#419EBB',
+          DEFAULT: '#2D68F0',
+          dark: '#070724',
+        },
+        grey: {
+          DEFAULT: '#838391',
+          dark: '#38384F',
+        },
+        yellow: {
+          DEFAULT: '#EDA249',
+        },
+        purple: {
+          DEFAULT: '#6D2ED6',
+        },
+        red: {
+          DEFAULT: '#D83A34',
+        },
+        orange: {
+          light: '#D14C32',
+          DEFAULT: '#CD5120',
+        },
+        aqua: {
+          DEFAULT: '#1EC1A2',
+        }
+
+      }
+    },
   },
   plugins: [],
 }
